0fc9b04072b70b1f259dcf167bf6b9133963e75c
Jane Venter <janeanneventer@gmail.com>
2019-02-12 05:15:22 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,7 +3,12 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.
+      t.string :theme 
+      t.float :price 
+      t.boolean :family_friendly 
+      t.string :opening_date
+      t.string :closing_date 
+      t.text :long_description
       
     end
   end
